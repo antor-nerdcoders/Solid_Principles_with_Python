a9a2ec756a31c1625821640d6ca24fd12b9e800a
antor-nerdcoders <noman@nerdcoders.xyz>
2025-02-17 21:42:14 +0600

    This is corrected file

--- open_closed_principle.py
+++ open_closed_principle.py
@@ -7,22 +7,22 @@ class Calculator:
 
 class Sum(Calculator):
     def display_sum(self):
-        return self.a + self.b
+        print("The Summation is ",self.a + self.b)
 
 
 class Sub(Calculator):
     def display_sub(self):
-        return self.a - self.b
+        print("The Subtraction is ",self.a - self.b)
 
 
 class Mul(Calculator):
     def display_mul(self):
-        return self.a * self.b
+        print("The Multiplication is ",self.a * self.b)
 
 
 class Div(Calculator):
     def display_div(self):
-        return self.a / self.b
+        print("The Division is ",self.a / self.b)
 
 calculator = int(input("Enter the type of operation\n"
                        "0 for summation\n"
@@ -32,7 +32,7 @@ calculator = int(input("Enter the type of operation\n"
 if calculator == 0:
     a = int(input("Enter first number to sum "))
     b = int(input("Enter second number to sum "))
-    obj_sum = Sum(a,b,0)
+    obj_sum = Sum(a, b,0)
     obj_sum.display_sum()
 
 
@@ -45,7 +45,7 @@ elif calculator == 1:
 elif calculator == 2:
     a = int(input("Enter first number to multiply "))
     b = int(input("Enter second number to multiply "))
-    obj_mul = Mul(a,b,2)
+    obj_mul = Mul(a, b,2)
     obj_mul.display_mul()
 else:
     a = int(input("Enter first number to div "))
